8cfe39358154e741367bf8881b51b33940b60a42
zhangxusheng <1024694790@qq.com>
2021-11-21 19:28:37 +0800

    feat: 第一个提交
    第一次提交
    
    BREAKING CHANGE: 第一次提交

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+declare const a = '1';
+declare const b = '2';
+declare function add(): string;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// const a = '1';
+// const b = '2';
+// function add(): string {
+//     return a + b;
+// }
+// console.log('a', a, b);
+// add();
